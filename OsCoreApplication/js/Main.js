b5fa7f91960f4748b8cd30d383008133a93db85c
anhnt9 <anh120991@gmail.com>
2021-01-14 00:05:55 +0700

    aaaaa

---
+++ OsCoreApplication/js/Main.js
@@ -0,0 +1,21 @@
+﻿
+
+function showToptip(tagFocus, tagNotify, tagTrans, value, pos, type, time)
+{
+    $(tagFocus).focus();
+    $(tagNotify).notify(
+                    value,
+                    {
+                        position: pos,
+                        className: type,
+                        autoHideDelay: time,
+                    }
+                );
+    $(tagTrans).transition('shake');
+}
+
+function ShowModal(e) {
+    $(e).modal({
+        dimmerSettings: { opacity: 0.5 }
+    }).modal('setting', 'closable', false).modal('setting', 'duration', 200).modal('show');
+}
